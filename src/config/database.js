61ecafbcff0900b9e51846de670f0f6fb878f939
Willian Kaneta <tetsuokaneta@gmail.com>
2020-02-22 15:20:21 -0400

    Modulo 02 BootCamp GoStack - Go Barber Back-end

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'trbcsc.11',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
